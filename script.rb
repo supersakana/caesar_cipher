1e1f48afff361b8a135a0e65cd6cf33ef3bb899e
supersakana <zacwilliamson@icloud.com>
2022-02-16 12:11:12 -0600

    casear method now takes capital letters

--- script.rb
+++ script.rb
@@ -1,18 +1,20 @@
-require 'pry-byebug' 
 # ruby ./script.rb
-# binding.pry
 
 def caesar(str, number)
-  lower_alpha = ('a'..'z').to_a 
-  upper_alpha = ('A'..'Z').to_a
-  code = []
-
-  str.split('').each do |char|
-    (lower_alpha.include? char) || (upper_alpha.include? char) ? 
-    code.push(number + char.ord) : 
-    code.push(char)
+    lower_alpha = ('a'..'z').to_a 
+    upper_alpha = ('A'..'Z').to_a 
+    code = []
+  
+    str.split('').each do |char|
+      if lower_alpha.include? char
+        code.push(lower_alpha[lower_alpha.index(char) + number - lower_alpha.size])
+      elsif upper_alpha.include? char
+        code.push(upper_alpha[upper_alpha.index(char) + number - upper_alpha.size])
+      else
+        code.push(char)
+      end
+    end
+    puts code.join('')
   end
-  puts code
-end
-
-  caesar("Zac Williamson ", 4)
+  
+    caesar("My name is ~ Stranger !", 1)
