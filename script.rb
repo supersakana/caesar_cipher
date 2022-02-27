c7b8db1d7407f041b2c97279c964c6f211b66fd6
supersakana <zacwilliamson@icloud.com>
2022-02-27 17:12:43 -0600

    updated with rubocop

--- script.rb
+++ script.rb
@@ -1,16 +1,16 @@
 def caesar(str, number)
-    lower_alpha = ('a'..'z').to_a 
-    upper_alpha = ('A'..'Z').to_a 
-    code = []
-  
-    str.split('').each do |char|
-      if lower_alpha.include? char
-        code.push(lower_alpha[lower_alpha.index(char) + number - lower_alpha.size])
-      elsif upper_alpha.include? char
-        code.push(upper_alpha[upper_alpha.index(char) + number - upper_alpha.size])
-      else
-        code.push(char)
-      end
+  lower_alpha = ('a'..'z').to_a
+  upper_alpha = ('A'..'Z').to_a
+  code = []
+
+  str.split('').each do |char|
+    if lower_alpha.include? char
+      code.push(lower_alpha[lower_alpha.index(char) + number - lower_alpha.size])
+    elsif upper_alpha.include? char
+      code.push(upper_alpha[upper_alpha.index(char) + number - upper_alpha.size])
+    else
+      code.push(char)
     end
-    puts code.join('')
   end
+  puts code.join('')
+end
